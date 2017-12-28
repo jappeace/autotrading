da194d40f655670f899482f9e73a7ae9bebc563a
Jappie Klooster <jappieklooster@hotmail.com>
2017-12-28 13:05:10 +0700

    using asx for prices

--- analyze/main.py
+++ analyze/main.py
@@ -6,7 +6,8 @@ from collections import namedtuple
 from jinja2 import Template
 import logging
 import argparse
-from datetime import datetime
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
 
 
 url = "http://financials.morningstar.com/ajax/exportKR2CSV.html?t=%(exchange)s:%(ticker)s"
@@ -54,10 +55,6 @@ RateResult = namedtuple('RateResult', [
     'eps_valuation'
 ])
 
-def chunks(array, chunksize):
-    """Yield successive n-sized chunks from l."""
-    for i in range(0, len(array), chunksize):
-        yield array[i:i + chunksize]
 
 Realtime = namedtuple('Realtime', [
     'ticker',
@@ -68,35 +65,37 @@ Realtime = namedtuple('Realtime', [
     'timestamp'
 ])
 
-def find_realtime_stock(session, ticker):
-    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=%(ticker)s.AX&interval=1min&apikey=%(key)s'
-    api_key = ""
-    with open('key') as keyfile:
-        api_key = keyfile.readline()
-    formated_uri = url % {
-        'ticker': ticker,
-        'key': api_key
-    }
-    logging.debug(formated_uri)
-    response = session.get(formated_uri)
-    son = json.loads(response.text)
-    dict_key = "Time Series (1min)"
-    if dict_key not in son:
-        return None
-    series = son[dict_key]
-    collected = [(datetime.strptime(time, "%Y-%m-%d %H:%M:%S"), data) for time, data in series.items()]
-    sort = sorted(collected, key=lambda a: a[0])
+def chunks(array, chunksize):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(array), chunksize):
+        yield array[i:i + chunksize]
 
-    latest = sort[-1]
-    logging.debug(latest)
-    return Realtime(
-        timestamp=latest[0],
-        ticker=ticker,
-        price=latest[1]['4. close'],
-        high=latest[1]['2. high'],
-        low=latest[1]['3. low'],
-        volume=latest[1]['5. volume'],
-    )
+def find_realtime_stocks(session, *tickers):
+    url = 'http://www.asx.com.au/asx/markets/equityPrices.do?by=asxCodes&asxCodes=%s'
+    chunked = chunks(tickers, 10)
+    result = {}
+    for chunk in chunked:
+        formated_uri = url % '+'.join(chunk)
+        response = session.get(formated_uri)
+        soup = BeautifulSoup(response.text)
+        rows = soup.select('.datatable tr')[1:]
+        for row in rows:
+            ticker = row.select('.row a')[0].get_text()
+            colls = row.select('td')
+            price = colls[0].get_text().strip()
+            if not price:
+                continue
+            result[ticker] = Realtime(
+                ticker=ticker,
+                price=price,
+                high=colls[6].get_text().strip(),
+                low=colls[7].get_text().strip(),
+                volume=colls[8].get_text().strip(),
+                # api is 20 minutes old
+                timestamp=datetime.now() - timedelta(minutes=20)
+            )
+    return result
+
 
 def float_or_zero(string):
     return float(string) if string else 0
@@ -112,6 +111,7 @@ def valuate_anuity(sequence, discount=0.9, count=1):
 def calculate_rating(data):
     eps_ann = valuate_anuity(data.eps)
     div_ann = valuate_anuity(data.dividends)
+    logging.debug(data.realtime)
     price = float(data.realtime.price)
     if price == 0.0:
         return None
@@ -182,6 +182,7 @@ def main():
         tickers = [row[1] for row in nohead]
 
     result = []
+    realtimes = find_realtime_stocks(session, *tickers)
     for ticker in tickers:
         logging.info('doing %s' % ticker)
         financial = get_financial(session, ticker)
@@ -194,7 +195,7 @@ def main():
         try:
             data = ShareData(
                 ticker=ticker,
-                realtime=find_realtime_stock(session, ticker),
+                realtime=realtimes.get(ticker),
                 eps=reversed(csv[8][1:-1]),
                 dividends=reversed(csv[9][1:-1])
             )
@@ -203,7 +204,6 @@ def main():
 
         if not data.realtime:
             continue
-
         rating = calculate_rating(data)
 
         if rating:
